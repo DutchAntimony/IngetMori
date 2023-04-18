c41bb491a14f4e2feed956ef0afaa89f6fd77fe1
Romke Veenstra <romkefritsveenstra@gmail.com>
2023-04-18 13:48:20 +0200

    Added FirebaseAuthProviderExtensions for claims

---
+++ src/IngetMori.Api/FirebaseAuthentication/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Claims;
+using System.Text.Json;
+
+namespace IngetMori.Api.FirebaseAuthentication;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static string GetFireBaseAuthEmailAddress(this ClaimsPrincipal principal)
+    {
+        var firebaseClaim = principal.Claims.FirstOrDefault(c => c.Type == "firebase")?.Value ?? "not found";
+        var firebaseAuthResult = JsonSerializer.Deserialize<FirebaseAuthResult>(firebaseClaim);
+        return firebaseAuthResult?.GetEmail() ?? "Not found";
+    }
+
+#pragma warning disable IDE1006 // Naming Styles -- justification: json deserialization
+    public record FirebaseAuthResult(Identities identities, string sign_in_provider)
+    {
+        public string? GetEmail() => identities?.email?.FirstOrDefault();
+    }
+
+    public record Identities(string[] email);
+#pragma warning restore IDE1006 // Naming Styles -- justification: json deserialization
+}
